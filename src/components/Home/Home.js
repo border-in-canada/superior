2c30bd06149bbfc91ff7233b603b1fac8f3bc767
David Mathes <mathesd84@gmail.com>
2019-12-15 15:30:49 -0800

    Added homepage mobile responsiveness

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,17 +13,17 @@ const home = (props) => {
             <div className={styles.Heading}>
                 <h2>Superior Service</h2>
                 <h1>Superior Recovery</h1>
-                <h3>503-253-0571</h3>
+                <h3><a href="tel:503-253-0571">503-253-0571</a></h3>
             </div>
             <div className={styles.Content}>
+                <div className={styles.Logo}>
+                    <img src={logo}/>  
+                </div> 
                 <div className={styles.Creds}>
                     <h4>CARS Certified</h4>
                     <h4>Licensed</h4>
                     <h4>Bonded & Insured</h4>
                 </div>
-                <div className={styles.Logo}>
-                    <img src={logo}/>  
-                </div> 
                 <div className={styles.Footer}>
                     <h4>Servicing the Portland and surrounding area since 1999.</h4><br />
                     <p><a href="">Sign in</a> to dispatch an order, view status updates, or view past orders.</p><br />
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import styles from './Toolbar.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faPhone, faSignInAlt } from '@fortawesome/free-solid-svg-icons';
+import logo from '../../../assets/Superior-Grey-Cropped.png';
 
-const toolbar = (props) => (
+const toolbar = (props) => {
+    
+    return(
     <nav className={styles.Toolbar}>
-        <h3 style={{paddingLeft: "1em"}} >Call or Email for pricing!</h3>
-        <p>mike@superiorrecoveryllc.com  |  503-253-0571</p>
+        {window.screen.width >= 768 ?
+        <div className={styles.Logo}><img src={logo}/></div> :
+        null}
         <div className={styles.NavItems}>
             <ul>
-                <li><button href="">Login</button></li>
+                <li><a href="mailto:mike@superiorrecoveryllc.com"><FontAwesomeIcon icon={faEnvelope} size='lg'/></a></li>
+                <li><a href="tel:503-253-0571"><FontAwesomeIcon icon={faPhone} size="lg"/></a></li>
+                <li><a href=""><FontAwesomeIcon icon={faSignInAlt} size="lg" /></a></li>
             </ul>
         </div>
     </nav>
-);
+    );
+};
 
 
 export default toolbar;
