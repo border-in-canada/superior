b29207772101bbed00956ab629fd585aa63ef607
David Mathes <mathesd84@gmail.com>
2020-08-10 11:07:36 -0700

    L

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
     let routes = (
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route path="/auth/:code" component={AuthLoader} />
+        <Route path="/auth" component={AuthLoader} />
         <Redirect to="/" />
       </Switch>
     );
