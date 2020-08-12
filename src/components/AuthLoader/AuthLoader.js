9abd1a94e519edf278e747b7dd4fa98809478468
David Mathes <mathesd84@gmail.com>
2020-08-11 21:35:16 -0700

    L

--- src/components/AuthLoader/AuthLoader.js
+++ src/components/AuthLoader/AuthLoader.js
@@ -35,9 +35,9 @@ class AuthLoader extends Component {
 
     render() {
 
-        if ((!this.props.location.hash && !this.props.location.search)) {
-            return <Redirect to="/" />
-        }
+        // if ((!this.props.location.hash && !this.props.location.search)) {
+        //     return <Redirect to="/" />
+        // }
 
         return (
             <div className={styles.Container}>
